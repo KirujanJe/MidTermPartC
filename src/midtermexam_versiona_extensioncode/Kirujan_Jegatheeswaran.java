f43d60e70b1425ff031bb3b5c0ac20770aa9c4fc
Kirujan Jegatheeswaran <jegathki@sheridancollege.ca>
2023-02-24 12:20:33 -0500

    midterm que 4, Kirujan

---
+++ src/midtermexam_versiona_extensioncode/Kirujan_Jegatheeswaran.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author kiruj
+ */
+public class Kirujan_Jegatheeswaran {
+    //Hello, this is Kirujan 
+    // for my midterm part B, question 4
+    //Thank you
+}
